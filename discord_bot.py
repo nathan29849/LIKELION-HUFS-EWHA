8e48ebd8c213591712107e3aa3d3de6e4a99ad53
nathan29849 <phs960418@gmail.com>
2021-09-28 00:55:09 +0900

    Discord Bot

---
+++ discord_bot.py
@@ -0,0 +1,34 @@
+import discord
+import random
+from discord.ext import commands
+
+ 
+bot = commands.Bot(command_prefix="!")
+
+# command -> 시작 command !시작 랜덤 command!랜덤
+# event -> on_massage, on_reactions_add
+
+@bot.command()
+async def 시작(ctx):
+    await ctx.send("안녕! 안녕! 안녕!")
+
+@bot.command()
+async def 배고파(ctx):
+    await ctx.send("친구한테 빌붙으세요")
+
+@bot.command()
+async def 랜덤(ctx):
+    # 1~100까지 숫자 중 하나를 랜덤으로 뽑기
+    number = random.randrange(1, 101)
+    await ctx.send(f"뽑힌 숫자: {number}")
+
+@bot.event
+async def on_message(message):
+    if message.author.bot == False:
+        await message.channel.send("누군가가 메시지를 입력했습니다!")
+
+@bot.event
+async def on_reaction_add(reaction, user):
+    await user.send("네가.. 이모티콘을 눌렀니..?")
+
+bot.run("ODkxOTk4MjY0Nzg0NzE5OTAz.YVGgHg.ExUK8mOMcVzcE8lCiMQwDPJO-oY")
